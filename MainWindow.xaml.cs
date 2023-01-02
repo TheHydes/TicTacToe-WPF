a5b28f5164ad2b05beddf86344c30981c4adab0d
TheHydes <takizsozsi@gmail.com>
2023-01-02 16:33:40 +0100

    Splitting "Window_MouseDown" into different methods, to make the program more transparent.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,8 +24,8 @@ namespace TicTacToe
         private string O = "O"; //elhagyható, felhasznált helyeken elég lenne helyette "O"
         private int counter = 0;
         private int counter2 = 0;
-        private string[] tomb;// = new string[9]; //esetleg érdemes lehet egy mátrixos megvalósításon elgondolkodni
-                                                  //a dinamikusság érdekében egy foreach-el megszámolom a myGrid gyerekelemeit és ezt adom meg a tömb méretének
+        private string[] tomb;// = new string[9]; //esetleg érdemes lehet egy mátrixos megvalósításon elgondolkodni                                      
+                              //a dinamikusság érdekében egy foreach-el megszámolom a myGrid gyerekelemeit és ezt adom meg a tömb méretének
         public MainWindow()
         {
             InitializeComponent();
@@ -38,13 +38,16 @@ namespace TicTacToe
             }
             tomb = new string[counter2];
         }
-
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            
-            if (counter<=8)
+            TombFeltoltes(e);
+            Vizsgalat();
+        }
+        private void TombFeltoltes(MouseButtonEventArgs e)
+        {
+            if (counter <= 8)
             {
-                if (counter % 2==0)
+                if (counter % 2 == 0)
                 {
                     if ((e.Source is Label) && (e.Source as Label).Content == null)
                     {
@@ -52,7 +55,6 @@ namespace TicTacToe
                         tomb[int.Parse((e.Source as Label).Tag.ToString())] = (e.Source as Label).Content.ToString();
                         counter++;
                     }
-                    
                 }
                 else
                 {
@@ -62,15 +64,17 @@ namespace TicTacToe
                         tomb[int.Parse((e.Source as Label).Tag.ToString())] = (e.Source as Label).Content.ToString();
                         counter++;
                     }
-                    
                 }
             }
+        }
+        private void Vizsgalat()
+        {
             if (counter >= 5) //innentől van értelme elkezdeni vizsgálni, hiszen ezelőtt még nem lehet nyerni
                               //, azért döntöttem a kézi eredménymegadás mellet, mert egyrészt kevés esetről van szó, másrészt a szomszédsági vizsgálatoknál hármassával, elakartam kerülni azon eseteket
                               //, mint például a tomb[2]=tomb[3]=tomb[4], hiszen ez egy kivételes eset ahol a szomszédok ugyanazok, viszont a játék szabályai szerint ez nem győzelem
                               //javítási lehetőség, hogy dinamikusabb legyen: valamilyen Backtrack algoritmus használata
             {
-                if ((tomb[0]==tomb[1] && tomb[0]==tomb[2] && tomb[0]==X) || (tomb[0] == tomb[3] && tomb[0] == tomb[6] && tomb[0] == X) || (tomb[0] == tomb[4] && tomb[0] == tomb[8] && tomb[0] == X)
+                if ((tomb[0] == tomb[1] && tomb[0] == tomb[2] && tomb[0] == X) || (tomb[0] == tomb[3] && tomb[0] == tomb[6] && tomb[0] == X) || (tomb[0] == tomb[4] && tomb[0] == tomb[8] && tomb[0] == X)
                     || (tomb[2] == tomb[4] && tomb[2] == tomb[6] && tomb[2] == X) || (tomb[1] == tomb[4] && tomb[1] == tomb[7] && tomb[1] == X) || (tomb[2] == tomb[5] && tomb[2] == tomb[8] && tomb[2] == X)
                     || (tomb[3] == tomb[4] && tomb[3] == tomb[5] && tomb[3] == X) || (tomb[6] == tomb[7] && tomb[6] == tomb[8] && tomb[6] == X))
                 {
@@ -84,7 +88,7 @@ namespace TicTacToe
                     MessageBox.Show("O nyert!");
                     this.Close();
                 }
-                else if (counter==9)
+                else if (counter == 9)
                 {
                     MessageBox.Show("Döntetlen!");
                     this.Close();
